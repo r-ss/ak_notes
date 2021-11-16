4efd31e8fc49a7e5c47af1a856fdaeb25450a692
Alex Antipov <mail@ress.ws>
2021-11-16 09:40:23 +0300

    categories CRUD views closed for unauthorized

--- src/views/categories.py
+++ src/views/categories.py
@@ -1,10 +1,13 @@
 from fastapi_utils.cbv import cbv
-from fastapi import status
+from fastapi import status, Depends
 from fastapi.responses import JSONResponse
 from fastapi_utils.inferring_router import InferringRouter
 
+from models.user import UserTokenBM
 from models.category import CategoryBM, CategoriesBM
+
 from services.category import CategoryCRUD
+from services.users.auth import token_required, owner_or_admin_can_proceed_only
 
 router = InferringRouter()
 
@@ -14,14 +17,14 @@ class CategoriesCBV:
 
     """ CREATE """
     @router.post('/categories', status_code=status.HTTP_201_CREATED)
-    def category_create(self, category: CategoryBM):
+    def category_create(self, category: CategoryBM, token: UserTokenBM = Depends(token_required)):
         db_category = CategoryCRUD.create(category.name)
         category = CategoryBM.parse_raw(db_category.to_json())
         return category
 
     """ READ """
     @router.get('/categories/{numerical_id}')
-    def category_read(self, numerical_id: int):
+    def category_read(self, numerical_id: int, token: UserTokenBM = Depends(token_required)):
         db_category = CategoryCRUD.read_specific(numerical_id)
 
         if not db_category:
@@ -34,21 +37,21 @@ class CategoriesCBV:
         return category
 
     @router.get('/categories')
-    def category_read_all(self):
+    def category_read_all(self, token: UserTokenBM = Depends(token_required)):
         db_categories = CategoryCRUD.read_all()
         categories = CategoriesBM.parse_raw(db_categories.to_json())
         return categories
 
     """ UPDATE """
     @router.put('/categories/{numerical_id}')
-    def category_update(self, numerical_id: int, category: CategoryBM):
+    def category_update(self, numerical_id: int, category: CategoryBM, token: UserTokenBM = Depends(token_required)):
         db_category = CategoryCRUD.update(numerical_id, category)
         category = CategoryBM.parse_raw(db_category.to_json())
         return category
 
     """ DELETE """
     @router.delete('/categories/{numerical_id}', status_code=status.HTTP_204_NO_CONTENT)
-    def category_delete(self, numerical_id: int):
+    def category_delete(self, numerical_id: int, token: UserTokenBM = Depends(token_required)):
         db_category = CategoryCRUD.read_specific(numerical_id)
         category = CategoryBM.parse_raw(db_category.to_json())
         CategoryCRUD.delete(numerical_id)
